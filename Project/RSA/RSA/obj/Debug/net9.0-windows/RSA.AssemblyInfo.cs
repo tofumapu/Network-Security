9be58e718ae4c5f8258dfa6fe166154ad6d3a116
Tofu <dangka3789@gmail.com>
2025-06-18 07:39:19 +0700

    Update commit

--- Project/RSA/RSA/obj/Debug/net9.0-windows/RSA.AssemblyInfo.cs
+++ Project/RSA/RSA/obj/Debug/net9.0-windows/RSA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a936a5bed4a83b12b012e7855435065144f99a61")]
 [assembly: System.Reflection.AssemblyProductAttribute("RSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
